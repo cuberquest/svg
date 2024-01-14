92d259bfbd5ac8877f5a4682c66dce4e0aebc98b
cuberquest <134105195+cuberquest@users.noreply.github.com>
2024-01-13 22:39:47 -0500

    Making `<header>` stick to top and giving `div#scroll` functionality.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 for (const img of document.images) img.draggable = false;
+
+const scr = document.getElementById("scroll");
+scr.addEventListener("click", () => scroll({ top: 0, left: 0, behavior: "smooth" }));
