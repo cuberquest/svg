d3e6ab964b2ff5e9eb438c3e91e0d4cb221025c9
cuberquest <134105195+cuberquest@users.noreply.github.com>
2024-01-22 15:43:50 -0500

    Adding scroll from top to where you were before.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 for (const img of document.images) img.draggable = false;
 
+onload = () => {
+  localStorage.setItem("scrY", scrollY);
+  scroll(0, 0);
+  scroll(localStorage.getItem("scrY"));
+  addEventListener("scrollend", () => localStorage.setItem("scrY", scrollY));
+};
+
 const scr = document.getElementById("scroll");
-scr.addEventListener("click", () => scroll({ top: 0, left: 0, behavior: "smooth" }));
+scr.addEventListener("click", () => scroll(0, 0));
