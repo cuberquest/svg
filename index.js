90561ee730d89104d20f90189746abec81b1c4e0
cuberquest <134105195+cuberquest@users.noreply.github.com>
2024-10-22 10:27:56 -0400

    Automating SVG Box processes

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ onload = () => {
 };
 
 const scr = document.getElementById("scroll");
-scr.addEventListener("click", () => scroll(0, 0));
+scr.addEventListener("click", function() {
+  scroll(0, 0);
+});
+
+const images = [
+  { src: "img/logo.svg", desc: "Box Content 1" },
+  { src: "img/logo.svg", desc: "Box Content 2" },
+  { src: "img/logo.svg", desc: "Box Content 3" },
+  { src: "img/logo.svg", desc: "Box Content 4" },
+  { src: "img/logo.svg", desc: "Box Content 5" },
+  { src: "img/logo.svg", desc: "Box Content 6" },
+  { src: "img/logo.svg", desc: "Box Content 7" },
+  { src: "img/logo.svg", desc: "Box Content 8" },
+  { src: "img/logo.svg", desc: "Box Content 9" },
+  { src: "img/logo.svg", desc: "Box Content 10" },
+  { src: "img/logo.svg", desc: "Box Content 11" },
+  { src: "img/logo.svg", desc: "Box Content 12" }
+]
+
+for (let i = 0; i < 12; i++) {
+  document.getElementById("suggested").innerHTML += `<div class="svg-box"><a href><img src="${images[i].src}"><div class="desc">${images[i].desc}</div></a></div>\\`
+}
